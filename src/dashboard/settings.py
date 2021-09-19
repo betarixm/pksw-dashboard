b232b693dca9e8e3c7a607361862dfd8ddd0ace8
beta <beta.underalpha@gmail.com>
2021-09-20 00:31:18 +0900

    🗂 STATIC_ROOT

--- src/dashboard/settings.py
+++ src/dashboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ssu%ro53m67nia!64#w)+80)p62p%%a0(1er%vpmikdnqxh81e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
