fd317f33aabce55e478dca71ccd5939a791c0996
beta <beta.underalpha@gmail.com>
2021-09-19 20:13:41 +0900

    🔥 Hotfix: Serializer fix

--- src/team/serializer.py
+++ src/team/serializer.py
@@ -2,11 +2,11 @@ from rest_framework import serializers
 
 from game.serializer import TileSlotSerializer
 
-from models import Team
+from .models import Team
 
 
 class TeamSerializer(serializers.ModelSerializer):
-    slot = TileSlotSerializer(read_only=True)
+    slot = TileSlotSerializer(read_only=True, many=True)
 
     class Meta:
         model = Team
