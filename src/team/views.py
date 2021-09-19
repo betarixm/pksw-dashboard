13b07a1b2dd146454bddf07061d51b54e159f6bb
beta <beta.underalpha@gmail.com>
2021-09-19 20:14:32 +0900

    🥣 REST for Team

--- src/team/views.py
+++ src/team/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Team
+from .serializer import TeamSerializer
+
+
+class TeamView(generics.ListAPIView):
+    serializer_class = TeamSerializer
+    queryset = Team.objects.all()
+
+    def get_queryset(self):
+        queryset = Team.objects.all().order_by("id")
+        name = self.request.query_params.get("name")
+
+        if name:
+            queryset = queryset.filter(name=name)
+
+        return queryset
