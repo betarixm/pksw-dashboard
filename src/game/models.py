40ff5751d468cdd0885fd663f0384aab69fe34d5
beta <beta.underalpha@gmail.com>
2021-09-19 14:13:17 +0900

    🎮 (Game) Tile model impl.

--- src/game/models.py
+++ src/game/models.py
@@ -1,3 +1,13 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+class Tile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    title = models.CharField("이름", max_length=100)
+    latex = models.CharField("LaTex", max_length=100)
+
+    class Meta:
+        verbose_name = "타일"
+        verbose_name_plural = "타일들"
