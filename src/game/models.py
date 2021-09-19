808e5b7fe5a501a1d93b6710ddabfe471df7e80a
beta <beta.underalpha@gmail.com>
2021-09-19 14:17:26 +0900

    🎮 (Game) TileSlot model impl.

--- src/game/models.py
+++ src/game/models.py
@@ -11,3 +11,13 @@ class Tile(models.Model):
     class Meta:
         verbose_name = "타일"
         verbose_name_plural = "타일들"
+
+
+class TileSlot(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+
+    tile = models.OneToOneField(Tile, related_name="slot", on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = "슬롯"
+        verbose_name_plural = "슬롯들"
