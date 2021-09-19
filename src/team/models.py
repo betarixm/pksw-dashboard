fb9b301d909c1602409dce917c6f26271936f20b
beta <beta.underalpha@gmail.com>
2021-09-19 14:43:12 +0900

    😀 (Team) Team model impl.

--- src/team/models.py
+++ src/team/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    name = models.CharField("이름", max_length=1000)
+
+    class Meta:
+        verbose_name = "팀"
+        verbose_name_plural = "팀들"
+
+    def __str__(self):
+        return self.name
