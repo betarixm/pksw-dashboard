becfb0693a4cb8ffacb3765237db5ffd832dc6a5
happyhappy <bjyoon513@gmail.com>
2021-09-21 04:01:22 +0900

    📊 Model support for expression

--- src/game/models.py
+++ src/game/models.py
@@ -10,29 +10,44 @@ class Tile(models.Model):
 
     title = models.CharField("이름", max_length=100)
     latex = models.CharField("LaTex", max_length=100)
+    team = models.ForeignKey(Team, related_name="tile", on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
         verbose_name = "타일"
         verbose_name_plural = "타일들"
+        ordering = ["team"]
 
     def __str__(self):
-        return f"({str(self.slot.team) if hasattr(self, 'slot') and self.slot.team else 'DECK'}) {self.title}"
+        return f"({str(self.team) if hasattr(self, 'team') and self.team else 'DECK'}) {self.title}"
 
 
-class TileSlot(models.Model):
+class Expression(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField("제목", max_length=100)
+
+    team = models.ForeignKey(Team, related_name="expression", on_delete=models.CASCADE)
+    is_completed = models.BooleanField("완성 여부")
+
+    class Meta:
+        verbose_name = "수식"
+        verbose_name_plural = "수식들"
+        ordering = ["team__name", "is_completed"]
+
+    def __str__(self):
+        return f"({str(self.team)}) {self.title}"
+
+
+class ExpressionSlot(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
+    expression = models.ForeignKey(Expression, related_name="slot", on_delete=models.CASCADE, null=True, blank=True)
+    order = models.PositiveSmallIntegerField(default=0, blank=False, null=False)
+
     tile = models.OneToOneField(
-        Tile, related_name="slot", on_delete=models.CASCADE, blank=True, null=True
-    )
-
-    team = models.ForeignKey(
-        Team, related_name="slot", on_delete=models.CASCADE, blank=True, null=True
-    )
-
-    is_allocated = models.BooleanField("배치 여부")
-
-    order = models.PositiveBigIntegerField(default=0, blank=False, null=False)
+        Tile, related_name="slot", on_delete=models.CASCADE, blank=True, null=True,
+        limit_choices_to={
+            "team__isnull": False,
+        })
 
     class Meta:
         verbose_name = "슬롯"
@@ -40,4 +55,4 @@ class TileSlot(models.Model):
         ordering = ["order"]
 
     def __str__(self):
-        return f"({str(self.team) if self.team else 'DECK'}) {self.id}"
+        return f"({str(self.expression.team) if self.expression else 'DECK'}) {self.id}"
