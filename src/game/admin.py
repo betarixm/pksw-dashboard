b28d9127f1c5a6b313672f1de65b87763d62421c
happyhappy <bjyoon513@gmail.com>
2021-09-21 04:03:12 +0900

    🖥️ Admin console for expression

--- src/game/admin.py
+++ src/game/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
+from adminsortable2.admin import SortableInlineAdminMixin
 
-from .models import Tile
+from .models import Tile, ExpressionSlot, Expression
+
+
+class TileSlotInline(SortableInlineAdminMixin, admin.TabularInline):
+    model = ExpressionSlot
+    extra = 1
 
 
 @admin.register(Tile)
 class TileAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(Expression)
+class ExpressionAdmin(admin.ModelAdmin):
+    inlines = [TileSlotInline]
